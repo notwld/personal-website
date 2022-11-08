ad4338e2ac015fe551e3c48717314d23f52db01c
Muhammad Waleed <77747037+notwld@users.noreply.github.com>
2022-11-08 23:29:12 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ const Page = () => {
                 <LinkBox>
                     <Box mt={2}>
                         <FontAwesomeIcon icon={faGithub} color="purple" style={{ marginRight: 5 }} />
-                        <Link style={{ textDecoration: "none" }} href="https://www.github.com/blurryface92" target="_blank"><span>blurryface92</span></Link>
+                        <Link style={{ textDecoration: "none" }} href="https://www.github.com/notwld" target="_blank"><span>blurryface92</span></Link>
                     </Box>
                     <Box mt={2}>
                         <FontAwesomeIcon icon={faLinkedin} color="purple" style={{ marginRight: 5 }} />
